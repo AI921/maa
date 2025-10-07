a24a2b0433ff7c45848e99591887496ad697c672
AI921 <arathod0122003@gmail.com>
2025-10-07 19:24:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,67 +8,62 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const CLIENT_PATH = path.join(__dirname, "../client");
-app.use(express.static(CLIENT_PATH));
+app.use(express.static(path.join(__dirname, "../client")));
 
 const messagesFile = path.join(__dirname, "messages.json");
-let users = {};
-let messages = [];
-
-if (fs.existsSync(messagesFile)) {
-  messages = JSON.parse(fs.readFileSync(messagesFile));
-}
+let messages = fs.existsSync(messagesFile)
+  ? JSON.parse(fs.readFileSync(messagesFile))
+  : [];
 
 function saveMessages() {
   fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
 }
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+const users = {};
 
+io.on("connection", (socket) => {
   socket.on("login", (username) => {
     users[username] = socket.id;
     console.log(`${username} logged in`);
-    socket.emit("load_messages", messages);
+    const userMessages = messages.filter(
+      (m) =>
+        (m.sender === username && m.receiver !== username) ||
+        (m.receiver === username && m.sender !== username)
+    );
+    socket.emit("load_messages", userMessages);
   });
 
   socket.on("send_message", (data) => {
-    const { sender, receiver, message } = data;
     const newMsg = {
-      sender,
-      receiver,
-      message,
-      time: new Date().toLocaleTimeString(),
-      seen: false,
+      sender: data.sender,
+      receiver: data.receiver,
+      message: data.message,
+      time: new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+      read: false,
     };
     messages.push(newMsg);
     saveMessages();
-    const receiverSocket = users[receiver];
+
+    const receiverSocket = users[data.receiver];
     if (receiverSocket) {
       io.to(receiverSocket).emit("receive_message", newMsg);
     }
   });
 
-  socket.on("message_seen", (sender) => {
-    messages.forEach((msg) => {
-      if (msg.sender === sender) msg.seen = true;
+  socket.on("typing", (data) => {
+    const receiverSocket = users[data.receiver];
+    if (receiverSocket) io.to(receiverSocket).emit("typing", data.sender);
+  });
+
+  socket.on("read_messages", (reader) => {
+    messages.forEach((m) => {
+      if (m.receiver === reader) m.read = true;
     });
     saveMessages();
   });
 
-  socket.on("typing", (data) => {
-    const { sender, receiver, isTyping } = data;
-    const receiverSocket = users[receiver];
-    if (receiverSocket) {
-      io.to(receiverSocket).emit("show_typing", { sender, isTyping });
-    }
-  });
-
   socket.on("disconnect", () => {
-    for (const name in users) {
-      if (users[name] === socket.id) delete users[name];
-    }
-    console.log("User disconnected");
+    for (let name in users) if (users[name] === socket.id) delete users[name];
   });
 });
 
